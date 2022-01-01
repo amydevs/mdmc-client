d00928585452a50cf5a55a2bf88c4b5c858df728
amy <ctrlalternativedelete@gmail.com>
2022-01-01 20:59:50 +1100

    added settings

---
+++ src/views/Settings.vue
@@ -0,0 +1,16 @@
+<template>
+  
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+  export default Vue.extend({
+    name: 'Settings',
+    data() {
+      return {
+      }
+    },
+    async mounted() {
+    }
+  })
+</script>
