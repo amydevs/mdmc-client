3a73b5aed6c64c27d0de4c5c064310790a9051ae
amy <ctrlalternativedelete@gmail.com>
2022-01-01 21:05:05 +1100

    fixed up colors

--- src/App.vue
+++ src/App.vue
@@ -28,28 +28,8 @@
     
     <v-app-bar
       app
-      color="primary"
-      dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
       <v-spacer></v-spacer>
 
@@ -80,7 +60,7 @@ export default Vue.extend({
     PathDialogue
   },
   data: () => ({
-    drawer: true,
+    drawer: false,
     items: [
         {icon: 'mdi-home', title:'Home', route:'/'},
         {icon: 'mdi-tools', title:'Library', route:'/library'}
