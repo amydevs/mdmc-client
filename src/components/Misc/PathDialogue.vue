40be45f33c4a721ce4b945e7b6abb2c7401ade80
amy <ctrlalternativedelete@gmail.com>
2022-01-02 19:38:52 +1100

    fixed dialogue

--- src/components/Misc/PathDialogue.vue
+++ src/components/Misc/PathDialogue.vue
@@ -56,7 +56,7 @@
       }
     },
     async mounted() {
-      if (window.electron.fs.existsSync(window.electron.store.get("gamePath"))) {
+      if (!window.electron.fs.existsSync(window.electron.store.get("gamePath"))) {
         this.dialog = true
       }
     },
