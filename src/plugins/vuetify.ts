88b1bdd4dfe6acd6d0b8993d4b246df672d5e6e6
amy <ctrlalternativedelete@gmail.com>
2022-01-02 18:05:22 +1100

    changed default primary color

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,5 +4,15 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    theme: { dark: !window.electron.store.get('lightMode') }
+    theme: { 
+        dark: !window.electron.store.get('lightMode'),
+        themes: {
+            dark: {
+                primary: '#ff55c3',
+            },
+            light: {
+                primary: '#ff55c3',
+            }
+        }
+    }
 });
