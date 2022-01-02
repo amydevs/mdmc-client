8bc1f5c88a0d2f32160950a17619a10fb179952d
amy <ctrlalternativedelete@gmail.com>
2022-01-02 15:20:18 +1100

    added searching

--- src/components/Charts/ChartsContainer.vue
+++ src/components/Charts/ChartsContainer.vue
@@ -1,21 +1,56 @@
 <template>
+  <div>
   <v-container>
     <v-row class="justify-center">
-      <Chart v-for="(chart, i) in charts" :chart="chart" :key="i" />
+      <v-card width="100%" class="ma-3 pa-4">
+        <v-text-field
+            v-model="search"
+            label="Search"
+            outlined
+            clearable
+            hide-details
+          ></v-text-field>
+      </v-card>
+      <ChartComp v-for="(chart, i) in searchFilter" :chart="chart" :key="i" />
     </v-row>
   </v-container>
+  </div>
 </template>
 
 <script lang="ts">
   import Vue from 'vue'
-  import Chart from './Chart.vue'
+  import ChartComp from './Chart.vue'
+  import { Chart } from '@/types/chart'
 
   export default Vue.extend({
-    name: 'ChartsContainer',      
+    name: 'ChartsContainer',    
+    data() {
+      return {
+        search: '',
+      }
+    },
     components: {
-      Chart
+      ChartComp
     },
     props: ['charts'],
+    computed: {
+      searchFilter() {
+        const len = (this.$data as any).search.length as number
+        
+        var returnVal = this.charts as Chart[]
+        if (len !== 0) {
+          const lowercaseSearch = this.$data.search.toLowerCase()
+          returnVal = returnVal.filter(chart => {
+            return (
+              chart.name.toLowerCase().includes(lowercaseSearch) ||
+              chart.author.toLowerCase().includes(lowercaseSearch) ||
+              chart.levelDesigner.toLowerCase().includes(lowercaseSearch)
+            )
+          })
+        }
 
+        return returnVal
+      }
+    }
   })
 </script>
