e8c5405b75b5a7e25a95dd7b8828b594e9befc6d
amy <ctrlalternativedelete@gmail.com>
2022-01-02 17:31:57 +1100

    added settings

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,30 @@
 <template>
-  
+  <v-container>
+    <v-card>
+      <v-list>
+        <template v-for="(setting, i) in settings">
+          <v-list-item :key="i">
+              <v-list-item-content>
+                <v-list-item-title>{{setting.title}}</v-list-item-title>
+                <v-list-item-subtitle>{{setting.subtitle}}</v-list-item-subtitle>
+
+                <v-text-field v-if="setting.type == 'directory'"
+                    placeholder="C:\Program Files\Steam\steamapps\common\Muse Dash\"
+                    class="mt-3"
+                    outlined
+                    append-icon="mdi-dots-horizontal-circle"
+                    :rules="setting.rules"
+                    v-model="setting.value"
+                    @click:append="openTDialog(setting)"
+                ></v-text-field>
+
+              </v-list-item-content>
+          </v-list-item>
+          <v-divider v-if="settings.length-1 !== i" :key="`divfor-${i}`"/>
+        </template>
+      </v-list>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -8,7 +33,33 @@
     name: 'Settings',
     data() {
       return {
+        settings: [
+          {
+            type: 'directory',
+            title: "Set Game Path",
+            subtitle: "Set Your Muse Dash Game Path Here",
+            rules: [
+              (v:string) => {
+                const existsBool = window.electron.fs.existsSync(v)
+                if (existsBool) {
+                  console.log("Set Game Path")
+                  window.electron.store.set("gamePath", v)
+                }
+                return existsBool || 'Game Path does not exist.'
+              }
+            ],
+            value: window.electron.store.get('gamePath'),
+          }
+        ]
       }
+    },
+    methods: {
+      async openTDialog(e:any) {
+        const folders = window.electron.dialog.openDialog()
+        if (folders) {
+          e.value = folders[0]
+        }
+      },
     },
     async mounted() {
     }
