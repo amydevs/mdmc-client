22b2f20d6afd79038fe9009f63ed62486585bc06
Amy Y <ctrlalternativedelete@gmail.com>
2022-01-03 12:34:29 +1100

    made auto grow song card

--- src/components/Charts/Chart.vue
+++ src/components/Charts/Chart.vue
@@ -1,7 +1,7 @@
 <template>
     <v-card
         width="500px"
-        class="ma-3 text-left"
+        class="ma-3 text-left flex-grow-1"
     >
     <div class="d-flex" style="height: 100%;">
         <v-avatar
