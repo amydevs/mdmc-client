aac852b6d843b37375f1975d9a9304b85be6c6b0
Amy Y <ctrlalternativedelete@gmail.com>
2022-01-04 00:59:31 +1100

    Revert "Update background.ts"
    This reverts commit 6cbf02dc72ee9475878a99da5380799a3b862c2d.

--- src/background.ts
+++ src/background.ts
@@ -37,7 +37,7 @@ async function createWindow() {
   } else {
     createProtocol('app')
     // Load the index.html when not in development
-    win.loadURL('app://./app:/')
+    win.loadURL('app://./index.html/')
   }
 }
 
