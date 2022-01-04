ba3be4593e2ff1ed542ca30363133d614238b038
Amy Y <ctrlalternativedelete@gmail.com>
2022-01-04 11:30:11 +1100

    added headers for downloads

---
+++ src/types/headers.ts
@@ -0,0 +1,15 @@
+export abstract class Headers {
+    public static DownloadHeaders = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://mdmc.moe/charts",
+        "Alt-Used": "mdmc.moe",
+        "Connection": "keep-alive",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "same-origin",
+        "TE": "Trailers"
+    }
+}
